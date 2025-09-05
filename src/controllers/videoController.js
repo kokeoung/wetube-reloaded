6a706cd41f09423f30371a513faa98fd40408e9e
kokeoung <145242344+runnerkk@users.noreply.github.com>
2025-09-05 22:40:13 +0900

    NodeJS-pug extends, mixin study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,41 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
+const fakeUser = {
+    username: "KK",
+    loggedIn: false,
+}
 
-export const edit = (req, res) => res.render("edit");
-
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 2,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 3,
+        },
+    ];
+    return res.render("home", {pageTitle: "Home", videos});
+}
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch Video"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const search = (req, res) => res.send("Search");
-
 export const upload = (req, res) => res.send("Upload");
-
 export const deleteVideo = (req, res) => {
     res.send("Delete");
 }
