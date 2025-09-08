acce8158bf963a5643fc7fd530776c754f4c8fbc
kokeoung <ko908599@gmail.com>
2025-09-08 22:09:26 +0900

    NodeJS get, post study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,14 +3,13 @@ const fakeUser = {
     loggedIn: false,
 }
 
-export const trending = (req, res) => {
-    const videos = [
+let videos = [
         {
             title: "First Video",
             rating: 5,
             comments: 2,
             createdAt: "2 minutes ago",
-            views: 59,
+            views: 1,
             id: 1,
         },
         {
@@ -30,12 +29,26 @@ export const trending = (req, res) => {
             id: 3,
         },
     ];
+
+export const trending = (req, res) => {
     return res.render("home", {pageTitle: "Home", videos});
-}
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch Video"});
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    res.send("Delete");
-}
+};
+
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+};
+
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+};
+
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title} = req.body;
+    videos[id-1].title = title;
+    res.redirect(`/videos/${id}`);
+};
--- src/server.js
+++ src/server.js
@@ -8,11 +8,10 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("tiny");
 
-console.log(process.cwd());
-
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views")
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
